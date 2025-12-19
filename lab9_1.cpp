93c1750231fa0c79a9176972e1dbcef60e44200b
prakao <kpkrisrin@gmail.com>
2025-12-19 15:11:03 +0700

    Lab9_1_Finish

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -3,10 +3,19 @@
 using namespace std;
 
 int main(){	
-	cout << "Enter initial loan: ";
-	cout << "Enter interest rate per year (%): ";
-	cout << "Enter amount you can pay per year: ";
 
+	double loan,rate,amount;
+	double PrevBalance,Interest,Total,Payment,NewBalance;
+	int Endofyear = 1;
+
+	cout << "Enter initial loan: ";
+	cin>>loan;
+	cout << "Enter interest rate per year (%): ";
+	cin>>rate;
+	cout << "Enter amount you can pay per year: ";
+	cin>>amount;
+
+	
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -17,17 +26,45 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+
+	PrevBalance = loan;
+	Payment = amount;
+
+	while(PrevBalance > 0){
+		Interest = PrevBalance*(rate/100);
+		Total = PrevBalance+Interest;
+
+		if(Total<=amount){
+			Payment = Total;
+		}
+		else{
+			Payment = amount;
+		}
+		NewBalance = Total-Payment;
+
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << Endofyear; 
+		cout << setw(13) << left << PrevBalance; 
+		cout << setw(13) << left << Interest; 
+		cout << setw(13) << left << Total;
+		cout << setw(13) << left << Payment;
+		cout << setw(13) << left << NewBalance;
+		cout << "\n";
+		
+		PrevBalance = NewBalance;
+		Endofyear ++;
+	}
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	// cout << fixed << setprecision(2); 
+	// cout << setw(13) << left << 1; 
+	// cout << setw(13) << left << 1000.0;
+	// cout << setw(13) << left << 50.0;
+	// cout << setw(13) << left << 1050.0;
+	// cout << setw(13) << left << 100.0;
+	// cout << setw(13) << left << 950.0;
+	// cout << "\n";	
 	
 	return 0;
 }
